cc759e1955aefca317b75727ed97a0ab522a3f9a
eedriz99 <akinsolaidris1999@gmail.com>
2025-12-21 17:52:47 +0000

    dupplicate validation function

--- main.go
+++ main.go
@@ -5,47 +5,6 @@ import (
 	"net/http"
 )
 
-//type server struct {
-//	port  string
-//	debug bool
-//}
-
-//	func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-//		switch r.URL.Path {
-//		case "/":
-//			switch r.Method {
-//			case http.MethodGet:
-//				w.Write([]byte("Hello World!"))
-//				return
-//			}
-//
-//		case "/profile":
-//			switch r.Method {
-//			case http.MethodGet:
-//				myProfile := User{
-//					FirstName: "Idris",
-//					LastName:  "Akinsola",
-//					Email:     "akinsolaidris1999@mail.com",
-//					Bio:       "AI/ML enthusiast, backend developer in making.",
-//				}
-//				w.Header().Set("Content-Type", "application/json; charset=utf-8")
-//				err := json.NewEncoder(w).Encode(myProfile)
-//				if err != nil {
-//					http.Error(w, err.Error(), http.StatusInternalServerError)
-//				}
-//				w.WriteHeader(http.StatusOK)
-//				//w.Write(myProfile)
-//				return
-//
-//			}
-//			return
-//
-//		//fmt.Fprintln("Request: %s", r.URL.Path)
-//		default:
-//			w.WriteHeader(http.StatusNotFound)
-//		}
-//	}
-
 func main() {
 
 	app := &app{port: ":8080", debug: true}
--- models/user.go
+++ models/user.go
@@ -6,31 +6,3 @@ type User struct {
 	Email     string `json:"email"`
 	Bio       string `json:"bio"`
 }
-
-//var users []User
-
-//func (u *User) GetUsers(w http.ResponseWriter, r *http.Request) {
-//	w.Header().Set("Content-Type", "application/json")
-//	err := json.NewEncoder(w).Encode(users)
-//	if err != nil {
-//		http.Error(w, err.Error(), http.StatusInternalServerError)
-//	} else {
-//		w.WriteHeader(http.StatusOK)
-//	}
-//	return
-//}
-
-//func isValidUser(u User) bool {
-//	if u.FirstName == "" || u.LastName == "" || u.Email == "" {
-//		return false
-//	} else {
-//		return true
-//	}
-//	//if u.FirstName == "" {
-//	//	return "Firstname is a required field!!"
-//	//} else if u.LastName == "" {
-//	//	return "Lastname is a required field!!"
-//	//} else if u.Email == "" {
-//	//	return "Email is a required field!!"
-//	//}
-//}
